b74777b20c391d54f5110cf24028cfa021d66023
dw9468-gamandoo <dw9468@gmail.com>
2026-03-31 16:57:09 +0900

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = 'boardnmystery-v1';
+const ASSETS = [
+  '/boardnmystery_pwa/',
+  '/boardnmystery_pwa/index.html',
+  '/boardnmystery_pwa/manifest.json'
+];
+
+// 설치 시 캐시 저장
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+// 활성화 시 구버전 캐시 삭제
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+// 네트워크 우선, 실패 시 캐시 반환
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request)
+      .then(res => {
+        const clone = res.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
+        return res;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
